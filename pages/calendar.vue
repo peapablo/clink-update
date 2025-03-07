affcb8c3ee6d6c8aac075b07171733dbbeddf0bd
Puvadet   Niyomdaychar <98137581+peapablo@users.noreply.github.com>
2025-03-07 16:37:50 +0700

    Create Calendar and Charts

---
+++ pages/calendar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <client-only>
+        <big-calendar></big-calendar>
+      </client-only>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineComponent } from 'vue';
+  import { useLayout } from '#app';
+  
+  useLayout('DashboardLayout');
+  </script>
